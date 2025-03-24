d493fbeaf6705d593fff9d3d086048fd71685e12
tanishpanchal1 <panchaltanish16@gmail.com>
2025-03-24 22:27:39 +0530

    first

---
+++ script.js
@@ -0,0 +1,37 @@
+document.getElementById('addButton').addEventListener('click', function() {
+    const taskInput = document.getElementById('taskInput');
+    
+    if (taskInput.value.trim() === "") {
+        alert("Please enter a task.");
+        return; 
+    }
+
+    const taskList = document.getElementById('taskList');
+    
+    
+    const listItem = document.createElement('li');
+    
+   
+    const taskText = document.createTextNode(taskInput.value);
+    
+    
+    const deleteButton = document.createElement('button');
+    
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'delete-button';
+    
+    
+    deleteButton.addEventListener('click', function() {
+        taskList.removeChild(listItem); 
+    });
+
+    
+    listItem.appendChild(taskText);
+    listItem.appendChild(deleteButton);
+    
+   
+    taskList.appendChild(listItem);
+    
+    
+    taskInput.value = "";
+});
